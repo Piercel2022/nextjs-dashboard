59ec4067d5f75931a8609eb0769b667370399ff8
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2024-06-28 17:07:41 +0200

    Add the workflow

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,5 @@
+
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -8,6 +10,8 @@ import {
 // Depending on the size of the application, this would be stored in a database.
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 const links = [
   { 
@@ -27,15 +31,22 @@ const links = [
 ];
 
 export default function NavLinks() {
+
+const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
           <Link
-            key={link.name}
-            href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          key={link.name}
+          href={link.href}
+          className={clsx(
+            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            {
+              'bg-sky-100 text-blue-600': pathname === link.href,
+            },
+          )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
